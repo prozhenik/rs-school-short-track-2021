052c8ed1264a2ad4574d9266c094d1c35f29287c
github-prozhenik <djfiers@gmail.com>
2021-04-12 23:40:47 +0300

    feat: encode-line completed

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let res = '';
+  let sum = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      sum += 1;
+    } else if (sum > 1) {
+      res += sum + str[i];
+      sum = 1;
+    } else {
+      res += str[i];
+    }
+  }
+  return res;
 }
 
 module.exports = encodeLine;
