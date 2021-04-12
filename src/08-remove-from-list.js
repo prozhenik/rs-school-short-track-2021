31a4666d2d7532e193c3095bed6618f6481838bf
github-prozhenik <djfiers@gmail.com>
2021-04-12 23:38:19 +0300

    feat: remove-from-list completed

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,18 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let list = l;
+  if (list.value === k && list.next === null) {
+    list = null;
+  } else if (list.next !== null) {
+    if (list.value === k) {
+      list = removeKFromList(list.next, k);
+    } else {
+      list.next = removeKFromList(list.next, k);
+    }
+  }
+  return list;
 }
 
 module.exports = removeKFromList;
